c096c770d4c1c98235d3991cf191468c006dbaba
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-02-26 00:03:58 -0500

    Initial commit

---
+++ source/actions.ts
@@ -0,0 +1,66 @@
+import { CacheType, CommandInteraction, Message, MessageEmbed } from "discord.js"
+import { REST } from "@discordjs/rest"
+import { Routes } from "discord-api-types/v9"
+
+import { name, version, author } from "../package.json"
+import Commands from "../data/commands.json"
+import Words from "../data/words.json"
+import Reactions from "../data/reactions.json"
+import Logger from "./class/logger"
+import Command from "./class/command"
+
+function embed() {
+	return new MessageEmbed({
+		color: 0xffee88,
+		footer: { text: `${name[0].toUpperCase() + name.slice(1).toLowerCase()} v${version} by ${author}` },
+	})
+}
+async function tempReply(interaction: CommandInteraction<CacheType>, reply: string) {
+	await interaction.reply(reply)
+	await new Promise((r) => setTimeout(r, 5000))
+	await interaction.deleteReply()
+}
+async function tempReplyEmbed(interaction: CommandInteraction<CacheType>, reply: MessageEmbed) {
+	await interaction.reply({ embeds: [reply] })
+	await new Promise((r) => setTimeout(r, 5000))
+	await interaction.deleteReply()
+}
+export function registerCommands(logger?: Logger) {
+	logger?.log(`Registering commands`)
+
+	Command.fromEntry(Commands[0], async (interaction) => {
+		await tempReplyEmbed(interaction, embed().setTitle("Pong!").setDescription("Monkey hears all 🙉"))
+		return true
+	})
+	Command.fromEntry(Commands[1], async (interaction) => {
+		await tempReply(interaction, `WIP`)
+		return true
+	})
+}
+export async function refreshDevCommands(clientID: string, logger?: Logger) {
+	const rest = new REST({ version: "9" }).setToken(process.env.TOKEN)
+	const route = Routes.applicationGuildCommands(clientID, process.env.DEVSERVER)
+
+	try {
+		await rest.put(route, { body: Commands })
+		logger?.log(`Refreshed dev commands`)
+	} catch (e) {
+		logger?.warn(`Error refreshing dev commands`)
+		logger?.warn(e)
+	}
+}
+export function react(message: Message, logger?: Logger) {
+	if (!Words.some((w) => message.content.toLowerCase().includes(w))) return
+
+	const weights: number[] = []
+	Reactions.forEach((r, i) => (weights[i] = r.weight + (weights[i - 1] ?? 0)))
+	const maxWeight = weights[weights.length - 1]
+	const random = Math.random() * maxWeight
+
+	for (const item of Reactions) {
+		if (weights[Reactions.indexOf(item)] < random) continue
+		message.react(item.emoji)
+		logger?.log(`Reacting to message! ${item.emoji}`)
+		break
+	}
+}
---
+++ source/main.ts
@@ -0,0 +1,40 @@
+import Discord from "discord.js"
+import DotEnv from "dotenv"
+
+import Config from "../data/config.json"
+import Logger from "./class/logger"
+import { react, refreshDevCommands, registerCommands } from "./actions"
+import Command from "./class/command"
+
+DotEnv.config()
+
+const logger = new Logger(Config.dev)
+const client = new Discord.Client({ intents: ["GUILD_MEMBERS", "GUILD_MESSAGES", "GUILD_MESSAGE_REACTIONS", "GUILDS"] })
+
+client.on("ready", () => {
+	logger.log(`Connected to API as ${client.user.tag}`)
+
+	if (Config.dev) {
+		logger.log(`Developer mode enabled 😎`)
+		refreshDevCommands(client.user.id, logger)
+	}
+
+	registerCommands(logger)
+
+	client.user.presence.set({
+		status: "idle",
+		activities: [{ type: "WATCHING", name: "for apes 👀" }],
+	})
+
+	logger.log(`Updated client presence`)
+})
+client.on("interactionCreate", async (interaction) => {
+	if (!interaction.isCommand()) return
+	logger.log(`Interaction recieved: "${interaction.commandName}"`)
+	Command.handle(interaction, logger)
+})
+client.on("messageCreate", (message) => {
+	react(message, logger)
+})
+
+client.login(process.env.TOKEN)
