0f445ed49db22d23015a3b1d7dcfe4bd70bb12ff
jaxydog <32988901+jaxydog@users.noreply.github.com>
2022-09-14 20:49:42 -0400

    Update string searching

--- source/utility/search.ts
+++ source/utility/search.ts
@@ -1,5 +1,5 @@
-export function str_contains(str: string, target: string | RegExp, rm_space = false, rm_case = true) {
-	if (rm_space) str = str.replace(/\s/g, "")
+export function str_contains(str: string, target: string | RegExp, rm_nontext = false, rm_case = true) {
+	if (rm_nontext) str = str.replace(/\W/g, "")
 	if (rm_case) str = str.toLowerCase()
 
 	return target instanceof RegExp ? target.test(str) : str.includes(target)
@@ -7,7 +7,7 @@ export function str_contains(str: string, target: string | RegExp, rm_space = fa
 export function object_contains_str(
 	obj: object,
 	target: string | RegExp,
-	rm_space = false,
+	rm_nontext = false,
 	rm_case = false,
 	max_d = 3,
 	d = 0
@@ -16,16 +16,16 @@ export function object_contains_str(
 
 	for (const val of Object.values(obj)) {
 		if (typeof val === "string") {
-			if (str_contains(val, target, rm_space, rm_case)) return true
+			if (str_contains(val, target, rm_nontext, rm_case)) return true
 		} else if (typeof val === "object" && val !== null) {
-			if (object_contains_str(val as any, target, rm_space, rm_case, max_d, d + 1)) return true
+			if (object_contains_str(val as any, target, rm_nontext, rm_case, max_d, d + 1)) return true
 		}
 	}
 
 	return false
 }
-export function get_contained(val: string | object, targets: (string | RegExp)[], rm_space = false, rm_case = false) {
+export function get_contained(val: string | object, targets: (string | RegExp)[], rm_nontext = false, rm_case = false) {
 	return typeof val === "string"
-		? targets.find((target) => str_contains(val, target, rm_space, rm_case))
-		: targets.find((target) => object_contains_str(val, target, rm_space, rm_case))
+		? targets.find((target) => str_contains(val, target, rm_nontext, rm_case))
+		: targets.find((target) => object_contains_str(val, target, rm_nontext, rm_case))
 }
